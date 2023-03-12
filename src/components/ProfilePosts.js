ef26f424366d3d66e6564ef9720d4f72935749d4
MichaelABryant <m.alexander.bryant@gmail.com>
2023-03-11 20:27:35 -0800

    Reverse order of profile posts

--- src/components/ProfilePosts.js
+++ src/components/ProfilePosts.js
@@ -4,7 +4,7 @@ function ProfilePosts(props) {
     return (
         <div className="feed-posts">
             {props.posts.length > 0 ? 
-                props.posts.map((post, index) => {
+                props.posts.slice(0).reverse().map((post, index) => {
                     return <Post author={props.account} post={post} key={index}/>
                 }) : null}
         </div>
