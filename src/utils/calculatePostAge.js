84515e80ab44fb0c3beb26ba11659747d7213edc
MichaelABryant <m.alexander.bryant@gmail.com>
2023-03-12 20:20:56 -0700

    Fix post age calculation

--- src/utils/calculatePostAge.js
+++ src/utils/calculatePostAge.js
@@ -2,19 +2,18 @@ function calculatePostAge(post) {
     let postDateTime = new Date(post.created_at)
     let currentDateTime = new Date(Date.now())
 
-    if ((currentDateTime.getFullYear() - postDateTime.getFullYear()) >= 1){
+    if ((postDateTime.getFullYear()-currentDateTime.getFullYear() ) >= 1){
         return String(currentDateTime.getFullYear() - postDateTime.getFullYear()) + "y"
     } else if ((currentDateTime.getMonth() - postDateTime.getMonth()) >= 1) {
         return String(currentDateTime.getMonth() - postDateTime.getMonth()) + "mo"
-    } else if ((currentDateTime.getDay() - postDateTime.getDay()) >= 1) {
-        return String(currentDateTime.getDay() - postDateTime.getDay()) + "d"
+    } else if ((currentDateTime.getDate() - postDateTime.getDate()) >= 1) {
+        return String(currentDateTime.getDate() - postDateTime.getDate()) + "d"
     } else if ((currentDateTime.getHours() - postDateTime.getHours()) >= 1) {
         return String(currentDateTime.getHours() - postDateTime.getHours()) + "h"
-    } else if ((currentDateTime.getMinutes() - postDateTime.getMinutes()) >= 1) {
-        return String(currentDateTime.getMinutes() - postDateTime.getMinutes()) + "m"
-    }else {
-        return "1m"
-    }
+    } else {
+        let minutes = Math.floor((currentDateTime.getTime() - postDateTime.getTime())/60000)
+        return String(minutes) + "m"
+        }
 }
 
 export {calculatePostAge};
