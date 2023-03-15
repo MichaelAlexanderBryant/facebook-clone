1fa859276a2ab04f56bfad61cd359863914890a0
MichaelABryant <m.alexander.bryant@gmail.com>
2023-03-15 15:51:20 -0700

    Add profile banner

--- src/components/ProfileHeader.js
+++ src/components/ProfileHeader.js
@@ -10,20 +10,27 @@ function ProfileHeader(props) {
         return (
             <div className="profile-header">
                 <div className="center-profile-header">
-                    <div className="left-side-profile-header">
-                        {props.profile.profile_picture ? 
-                        <img src={props.profile.profile_picture} alt="" className="profile-picture"/>
-                        : <img src={defaultProfilePicture} alt="" className="profile-picture"/>}
-                        <h1 className="profile-name">{props.profile.first_name + " " + props.profile.last_name}</h1>
-                    </div>
-                    <div className="right-side-profile-header">
-                        {String(user.user_id) === props.userId ? 
-                            <div><button type="button">Edit Profile</button></div>
-                            : <div>
-                                {userInfo.friends && userInfo.friends.includes(parseInt(props.userId)) ? 
-                                    <button type="button">Unfriend</button>
-                                    :<button type="button">Add Friend</button>}
-                            </div>}
+                    <div className="top-header"></div>
+                    <div className="bottom-header">
+                        <div className="left-side-profile-header">
+                            <div className="profile-img-container">
+                                <div className="circular-div">
+                                    {props.profile.profile_picture ? 
+                                    <img src={props.profile.profile_picture} alt="" className="profile-picture"/>
+                                    : <img src={defaultProfilePicture} alt="" className="profile-picture"/>}
+                                </div>
+                            </div>
+                            <h1 className="profile-name">{props.profile.first_name + " " + props.profile.last_name}</h1>
+                        </div>
+                        <div className="right-side-profile-header">
+                            {String(user.user_id) === props.userId ? 
+                                <div><button className="header-button" type="button">Edit Profile</button></div>
+                                : <div>
+                                    {userInfo.friends && userInfo.friends.includes(parseInt(props.userId)) ? 
+                                        <button className="header-button" type="button">Unfriend</button>
+                                        :<button className="header-button" type="button">Add Friend</button>}
+                                </div>}
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/ProfilePosts.js
+++ src/components/ProfilePosts.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import Post from "./Post";
 
 function ProfilePosts(props) {
